9b605e95ed2ff851cc804988506dccf364170878
Travis Illig <tillig@paraesthesia.com>
2026-05-22 10:02:01 -0700

    Convert from Moq to NSubstitute
    Replace Moq with NSubstitute 5.3.0 for consistency across Autofac
    repositories.

--- test/Autofac.Integration.Owin.Test/AutofacAppBuilderRunExtensionsFixture.cs
+++ test/Autofac.Integration.Owin.Test/AutofacAppBuilderRunExtensionsFixture.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Autofac Project. All rights reserved.
+// Copyright (c) Autofac Project. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 namespace Autofac.Integration.Owin.Test;
@@ -8,21 +8,21 @@ public class AutofacAppBuilderRunExtensionsFixture
     [Fact]
     public void RunFromContainerRequiresInjectorRegistrationFirst()
     {
-        var app = new Mock<IAppBuilder>();
-        app.Setup(mock => mock.Properties).Returns(new Dictionary<string, object>());
-        app.SetReturnsDefault(app.Object);
+        var app = Substitute.For<IAppBuilder>();
+        app.Properties.Returns(new Dictionary<string, object>());
+        app.Use(Arg.Any<object>(), Arg.Any<object[]>()).Returns(app);
 
-        Assert.Throws<InvalidOperationException>(() => app.Object.RunFromContainer<ITestComponent>((component, owinContext) => component.InvokeAsync(owinContext)));
+        Assert.Throws<InvalidOperationException>(() => app.RunFromContainer<ITestComponent>((component, owinContext) => component.InvokeAsync(owinContext)));
     }
 
     [Fact]
     public async Task RunFromContainerResolvesFromLifetimeScope()
     {
-        var instance = new Mock<ITestComponent>();
-        instance.Setup(c => c.InvokeAsync(It.IsAny<IOwinContext>())).Returns(Task.FromResult(0)).Verifiable();
+        var instance = Substitute.For<ITestComponent>();
+        instance.InvokeAsync(Arg.Any<IOwinContext>()).Returns(Task.FromResult(0));
 
         var builder = new ContainerBuilder();
-        builder.RegisterInstance(instance.Object);
+        builder.RegisterInstance(instance);
         using (var server = TestServer.Create(app =>
         {
             app
@@ -33,7 +33,7 @@ public class AutofacAppBuilderRunExtensionsFixture
             await server.HttpClient.GetAsync("/");
         }
 
-        instance.Verify();
+        await instance.Received(1).InvokeAsync(Arg.Any<IOwinContext>());
     }
 
     internal interface ITestComponent
